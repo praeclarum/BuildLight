0e3a63ae469d7074e602d73c9d58d968c018acc4
Frank A. Krueger <fak@praeclarum.org>
2020-06-14 12:45:07 -0700

    Add Alexa support for #4

--- BuildLightDevice/BuildLightDevice.ino
+++ BuildLightDevice/BuildLightDevice.ino
@@ -2,6 +2,7 @@
 #include <WiFiClient.h>
 #include <WebServer.h>
 #include <ESPmDNS.h>
+#include <fauxmoESP.h>
 
 // Step 1. Create SECTRET_* defines
 #include "Secrets.h"
@@ -12,7 +13,26 @@ const char* password = SECRET_PASSWORD;
 const int redPin = 27;
 const int greenPin = 26;
 
+uint8_t redIntensity = 0;
+uint8_t greenIntensity = 255;
+
+bool isOn = true;
+
 const char* host = "buildlight";
+
+#define ALEXA_ID "Build"
+
+void updateLight ()
+{
+  if (isOn) {
+      digitalWrite(redPin, redIntensity > 0 ? 0 : 1);
+      digitalWrite(greenPin, greenIntensity > 0 ? 0 : 1);
+  }
+  else {
+      digitalWrite(redPin, 1);
+      digitalWrite(greenPin, 1);
+  }
+}
 
 WebServer server(80);
 
@@ -41,9 +61,10 @@ void handleColor() {
       }
     }
   }
-  
-  digitalWrite(redPin, red > 0 ? 0 : 1);
-  digitalWrite(greenPin, green > 0 ? 0 : 1);
+
+  redIntensity = red;
+  greenIntensity = green;
+  updateLight();
   
   Serial.printf("SET COLOR red=%d, green=%d, blue=%d\n",
     (int)red, (int)green, (int)blue);
@@ -70,9 +91,13 @@ void setup(void) {
   digitalWrite(redPin, 1);
   pinMode(greenPin, OUTPUT);
   digitalWrite(greenPin, 1);
+  updateLight();
 
   Serial.begin(115200);
-  
+
+  //
+  // Connect to WiFi
+  //
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.println("");
@@ -91,6 +116,14 @@ void setup(void) {
     Serial.println("MDNS responder started");
   }
 
+  //
+  // Setup Alexa Server
+  //
+  setupAlexa ();
+
+  //
+  // Setup Color API Server
+  //
   server.on("/", handleIndex);
 
   server.on("/color", HTTP_POST, handleColor);
@@ -103,4 +136,36 @@ void setup(void) {
 
 void loop(void) {
   server.handleClient();
+}
+
+/* ======= ALEXA SUPPORT =============*/
+
+fauxmoESP fauxmo;
+
+#if CONFIG_FREERTOS_UNICORE
+#define ARDUINO_RUNNING_CORE 0
+#else
+#define ARDUINO_RUNNING_CORE 1
+#endif
+
+void setupAlexa()
+{
+    fauxmo.createServer(true); // not needed, this is the default value
+//    fauxmo.setPort(80); // This is required for gen3 devices
+    fauxmo.enable(true);
+    fauxmo.addDevice(ALEXA_ID);
+    fauxmo.onSetState([](unsigned char device_id, const char * device_name, bool state, unsigned char value) {
+        isOn = state;
+        Serial.println("Changed light from Alexa");
+        updateLight();
+    });
+    fauxmo.setState(ALEXA_ID, true, 255);
+    xTaskCreatePinnedToCore(loopBackground, "loopBackground", 4096, NULL, 1, NULL, ARDUINO_RUNNING_CORE);
+}
+
+void loopBackground(void *)
+{
+  for (;;) {
+    fauxmo.handle();
+  }
 }
