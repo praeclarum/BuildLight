249575395ae17d2fad7c4cdbdc5e210652ac5ae8
Frank A. Krueger <fak@praeclarum.org>
2020-07-19 12:29:25 -0700

    Trying to add OTA...fail

--- BuildLightDevice/BuildLightDevice.ino
+++ BuildLightDevice/BuildLightDevice.ino
@@ -2,6 +2,7 @@
 #include <WiFiClient.h>
 #include <WebServer.h>
 #include <ESPmDNS.h>
+#include <ArduinoOTA.h>
 
 #define DEBUG_FAUXMO                Serial
 #define DEBUG_FAUXMO_VERBOSE_TCP    1
@@ -125,6 +126,11 @@ void setup(void) {
   setupAlexa ();
 
   //
+  // Turn on OTA
+  //
+  setupOTA();
+
+  //
   // Setup Color API Server
   //
   server.on("/", handleIndex);
@@ -160,12 +166,65 @@ void setupAlexa()
         updateLight();
     });
     fauxmo.setState(ALEXA_ID, true, 255);
-    xTaskCreatePinnedToCore(loopBackground, "loopBackground", 4096, NULL, 1, NULL, ARDUINO_RUNNING_CORE);
+    xTaskCreatePinnedToCore(loopFauxmo, "loopBackground", 4096, NULL, 1, NULL, ARDUINO_RUNNING_CORE);
 }
 
-void loopBackground(void *)
+void loopFauxmo(void *)
 {
   for (;;) {
     fauxmo.handle();
+  }
+}
+
+void setupOTA() {
+  // Port defaults to 3232
+  // ArduinoOTA.setPort(3232);
+
+  // Hostname defaults to esp3232-[MAC]
+  String name = ALEXA_ID;
+  name += "_OTA";
+  ArduinoOTA.setHostname(name.c_str());
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+
+  xTaskCreatePinnedToCore(loopOTA, "loopOTA", 4096, NULL, 1, NULL, ARDUINO_RUNNING_CORE);
+
+  Serial.println("OTA Ready");
+}
+
+void loopOTA(void *)
+{
+  for (;;) {
+    ArduinoOTA.handle();
   }
 }
