5583a7fadec0d535587b928b44fdd287d7a29488
Alex Hedley <alex.hedley@me.com>
2020-10-03 09:04:10 +0100

    Removed win ver from sln

---
+++ BuildLightPlugin/Actions/SucceedAction.cs
@@ -0,0 +1,35 @@
+﻿using StreamDeckLib;
+using StreamDeckLib.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildLightPlugin
+{
+  [ActionUuid(Uuid="com.thefrankshow.buildlight.action.fail")]
+  public class FailAction : BaseStreamDeckActionWithSettingsModel<Models.CounterSettingsModel>
+  {
+	public override async Task OnKeyUp(StreamDeckEventPayload args)
+	{
+	  SettingsModel.Counter++;
+	  await Manager.SetTitleAsync(args.context, SettingsModel.Counter.ToString());
+	  
+	  //update settings
+	  await Manager.SetSettingsAsync(args.context, SettingsModel);
+	}
+
+	public override async Task OnDidReceiveSettings(StreamDeckEventPayload args)
+	{
+	  await base.OnDidReceiveSettings(args);
+	  await Manager.SetTitleAsync(args.context, SettingsModel.Counter.ToString());
+	}
+
+	public override async Task OnWillAppear(StreamDeckEventPayload args)
+	{
+	  await base.OnWillAppear(args);
+	  await Manager.SetTitleAsync(args.context, SettingsModel.Counter.ToString());
+	}
+
+  }
+}
---
+++ BuildLightPlugin/models/AnySettingsModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BuildLightPlugin.Models
+{
+  public class CounterSettingsModel
+  {
+	public int Counter { get; set; } = 0;
+  }
+}
